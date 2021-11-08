5aa30b2ee30cf0fab5b4992798b0bc6f8f63faf0
ColaGom <bchoi000@gmail.com>
2021-11-09 00:04:16 +0900

    Week 2, Layout modifiers under the hood

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/colagom/composelayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/colagom/composelayout/MainActivity.kt
@@ -14,19 +14,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import coil.compose.rememberImagePainter
 import com.colagom.composelayout.ui.theme.ComposeLayoutTheme
 import kotlinx.coroutines.launch
@@ -323,11 +320,20 @@ val topics = listOf(
 
 @Composable
 fun StaggerBody(modifier: Modifier = Modifier) {
-    StaggeredGrid(modifier = modifier.horizontalScroll(rememberScrollState())) {
-        topics.forEach {
-            Chip(modifier = Modifier.padding(8.dp), text = it)
+    Row(
+        modifier = modifier
+            .background(color = Color.LightGray)
+            .padding(16.dp)
+            .size(200.dp)
+            .horizontalScroll(rememberScrollState()),
+        content = {
+            StaggeredGrid {
+                topics.forEach {
+                    Chip(modifier = Modifier.padding(8.dp), text = it)
+                }
+            }
         }
-    }
+    )
 }
 
 @Preview
@@ -335,5 +341,42 @@ fun StaggerBody(modifier: Modifier = Modifier) {
 fun StaggeredPreview() {
     ComposeLayoutTheme {
         StaggerBody()
+    }
+}
+
+@Stable
+fun Modifier.padding(all: Dp) =
+    then(
+        PaddingModifier(start = all, top = all, end = all, bottom = all, rtlAware = true)
+    )
+
+private class PaddingModifier(
+    val start: Dp = 0.dp,
+    val top: Dp = 0.dp,
+    val end: Dp = 0.dp,
+    val bottom: Dp = 0.dp,
+    val rtlAware: Boolean,
+) : LayoutModifier {
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+
+        val horizontal = start.roundToPx() + end.roundToPx()
+        val vertical = top.roundToPx() + bottom.roundToPx()
+
+        val placeable = measurable.measure(constraints.offset(-horizontal, -vertical))
+
+        val width = constraints.constrainWidth(placeable.width + horizontal)
+        val height = constraints.constrainHeight(placeable.height + vertical)
+
+        return layout(width, height) {
+            if (rtlAware) {
+                placeable.placeRelative(start.roundToPx(), top.roundToPx())
+            } else {
+                placeable.place(start.roundToPx(), top.roundToPx())
+            }
+        }
     }
 }
