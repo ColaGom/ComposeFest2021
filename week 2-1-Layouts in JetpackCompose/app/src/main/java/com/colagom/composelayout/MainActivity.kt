c9f89992d38b8e87e604124a5eb82e4c5b5470c0
ColaGom <bchoi000@gmail.com>
2021-11-08 23:34:31 +0900

    Week 2, Complex custom layout

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/colagom/composelayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/colagom/composelayout/MainActivity.kt
@@ -3,9 +3,7 @@ package com.colagom.composelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -20,6 +18,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -184,7 +183,6 @@ fun Modifier.firstBaselistToTop(
     }
 )
 
-@Preview
 @Composable
 fun TextWithPaddingBaselinePreview() {
     ComposeLayoutTheme {
@@ -192,7 +190,6 @@ fun TextWithPaddingBaselinePreview() {
     }
 }
 
-@Preview
 @Composable
 fun TextWithPaddingNormalPreview() {
     ComposeLayoutTheme {
@@ -233,5 +230,110 @@ fun BodyContent(modifier: Modifier = Modifier) {
         Text(text = "BBBB")
         Text(text = "CCCC")
         Text(text = "DDDD")
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+
+            val placeable = measurable.measure(constraints)
+            val row = index % rows
+
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(
+            constraints.minWidth.rangeTo(constraints.maxWidth)
+        ) ?: constraints.minWidth
+
+        val height =
+            rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+
+            Spacer(Modifier.width(4.dp))
+            Text(text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    ComposeLayoutTheme {
+        Chip(text = "Hi there")
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun StaggerBody(modifier: Modifier = Modifier) {
+    StaggeredGrid(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        topics.forEach {
+            Chip(modifier = Modifier.padding(8.dp), text = it)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun StaggeredPreview() {
+    ComposeLayoutTheme {
+        StaggerBody()
     }
 }
